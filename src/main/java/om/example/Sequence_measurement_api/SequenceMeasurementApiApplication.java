50b4747abc10c3b4345f92c26a1847bbebcfd7f5
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-20 14:15:48 +0400

    initial structure of project

---
+++ src/main/java/om/example/Sequence_measurement_api/SequenceMeasurementApiApplication.java
@@ -0,0 +1,13 @@
+package om.example.Sequence_measurement_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SequenceMeasurementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SequenceMeasurementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/om/example/Sequence_measurement_api/SequenceMeasurementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package om.example.Sequence_measurement_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SequenceMeasurementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
